de411ec7b3316c2bfbc0ab000c47f64f62cd8346
Derek <derekchen1815@gmail.com>
2024-08-29 08:50:46 -0700

    adjustment

--- competition_code/drawWaypoints.py
+++ competition_code/drawWaypoints.py
@@ -25,37 +25,30 @@ plt.figure(figsize=(11, 11))
 plt.axis((-1100, 1100, -1100, 1100))
 plt.tight_layout()
 
+for waypoint in track[:] if track is not None else []:
+    rep_line = waypoint.line_representation
+    rep_line = np.asarray(rep_line)
+    waypoint_heading = tr3d.euler.euler2mat(*waypoint.roll_pitch_yaw) @ np.array(
+        [1, 0, 0]
+    )
+    plt.arrow(
+        waypoint.location[0],
+        waypoint.location[1],
+        waypoint_heading[0] * 1,
+        waypoint_heading[1] * 1,
+        width=0.5,
+        color="r",
+    )
+    plt.plot(rep_line[:, 0], rep_line[:, 1], "k")
+    progressBar.next()
 
-def plotTrack(track):
-    for waypoint in track[:] if track is not None else []:
-        rep_line = waypoint.line_representation
-        rep_line = np.asarray(rep_line)
-        waypoint_heading = tr3d.euler.euler2mat(*waypoint.roll_pitch_yaw) @ np.array(
-            [1, 0, 0]
-        )
-        plt.arrow(
-            waypoint.location[0],
-            waypoint.location[1],
-            waypoint_heading[0] * 1,
-            waypoint_heading[1] * 1,
-            width=0.5,
-            color="r",
-        )
-        plt.plot(rep_line[:, 0], rep_line[:, 1], "k")
-        progressBar.next()
+for i in waypoints:
+    plt.plot(i.location[0], i.location[1], "ro")
+    progressBar.next()
 
+# for i in additionalWaypoints:
+#     plt.plot(i.location[0], i.location[1], "g^")
 
-def drawWaypoints(waypointsBase, additionalWaypoints=waypoints[:1]):
-    for i in waypointsBase:
-        plt.plot(i.location[0], i.location[1], "ro")
-        progressBar.next()
-
-    for i in additionalWaypoints:
-        plt.plot(i.location[0], i.location[1], "g^")
-
-
-plotTrack(track)
-drawWaypoints(waypoints)
 progressBar.finish()
 print()
 plt.show()
